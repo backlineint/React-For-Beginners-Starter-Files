ecc3007d0144d7b81c6a65f2ee1cf775c3c9cf57
Brian Perry <contact.brianperry@gmail.com>
2017-07-16 13:51:16 -0400

    Chapter 3 examples

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+// Approach above allows us to import a single method from ReactDOM since we don't need the whole package.
+// import ReactDOM from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
