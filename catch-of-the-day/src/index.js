46158560668e53b929b106d3b4c0f85d55bd15eb
Brian Perry <contact.brianperry@gmail.com>
2017-07-17 08:49:25 -0400

    Chapter 5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 // Approach above allows us to import a single method from ReactDOM since we don't need the whole package.
 // import ReactDOM from 'react-dom';
+import './css/style.css'
 
 import StorePicker from './components/StorePicker';
 
