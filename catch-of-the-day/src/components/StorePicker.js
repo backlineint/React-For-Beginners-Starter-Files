fb5dcb7fae0aa6f11de8ba492f80b41c1cb11df1
Brian Perry <contact.brianperry@gmail.com>
2017-07-17 07:34:21 -0400

    Chapter 4

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      // Shortcut - type form.storeselector and let php storm autocomplete the jsx for you
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        { /* Can't use regular comments within JSX, have to do this */ }
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
 
