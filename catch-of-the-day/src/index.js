fc5d48ee295a9b832a6b08a43812d770c87c753c
Brian Perry <contact.brianperry@gmail.com>
2017-07-18 22:45:47 -0400

    Chapter 9

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { render } from 'react-dom';
 // Approach above allows us to import a single method from ReactDOM since we don't need the whole package.
 // import ReactDOM from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css'
 import App from './components/App';
 
-//import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App />, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector('#main'));
