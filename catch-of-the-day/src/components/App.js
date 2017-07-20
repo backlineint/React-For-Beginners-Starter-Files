b808f6c51152f057bc6b80287ca49fbeed55bd20
Brian Perry <contact.brianperry@gmail.com>
2017-07-20 07:49:50 -0500

    Chapter 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     // Can't use this keyword until we call super.
     super();
+
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+
     // Initial state / getInitialState (older approach)
     this.state = {
       fishes: {},
@@ -28,6 +32,12 @@ class App extends React.Component {
     // Line above is the same as this.setState({ fishes: fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +45,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
