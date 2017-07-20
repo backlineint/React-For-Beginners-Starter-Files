2279f310493f7036da1e90b7ce6257a7df217979
Brian Perry <contact.brianperry@gmail.com>
2017-07-19 22:09:03 -0500

    Chapter 13

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    // Can't use this keyword until we call super.
+    super();
+    this.addFish = this.addFish.bind(this);
+    // Initial state / getInitialState (older approach)
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // Update our state
+    // ... is a spread
+    const fishes = {...this.state.fishes};
+    // Not best practice, but could this.state.fishes.fish1 = fish
+    // Add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set state
+    this.setState({ fishes });
+    // Line above is the same as this.setState({ fishes: fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +35,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
