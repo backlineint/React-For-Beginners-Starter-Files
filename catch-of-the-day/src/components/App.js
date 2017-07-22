3b54276b5e2f0b87f94dcb177283d5900ad50b28
Brian Perry <contact.brianperry@gmail.com>
2017-07-22 10:34:24 -0500

    Chapter 17

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -67,7 +67,7 @@ class App extends React.Component {
             }
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         { /* Passsing methods down via props */ }
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
