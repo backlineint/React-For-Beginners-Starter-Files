8f5a38e35313d9468eb1ca721f225c0cbd923010
Brian Perry <contact.brianperry@gmail.com>
2017-07-23 09:32:53 -0500

    Chapter 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBsTk2b4S9eXcKO-qSnZpoGO8IaVBG2Jj0",
+  authDomain: "catch-of-the-day-brian-perry.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-brian-perry.firebaseio.com"
+});
+
+export default base;
