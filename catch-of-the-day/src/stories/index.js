275c05f19d77730f5e700e511ee936d110a77026
Brian Perry <contact.brianperry@gmail.com>
2017-07-26 15:42:26 -0500

    Initial configuration of Storybook

---
+++ catch-of-the-day/src/stories/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import '../css/style.css'
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import Header from '../components/Header';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+
+// Header component from Catch of the Day app, with Knobs enabled.
+storiesOf('Header', module)
+  .addDecorator(withKnobs)
+  .add('Header', () => <Header tagline={text('Tagline', 'Fresh Seafood Market')} />);
+
+const stories = storiesOf('Storybook Knobs', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
+
+// Knobs for React props
+stories.add('with a button', () => (
+  <button disabled={boolean('Disabled', false)} >
+    {text('Label', 'Hello Button')}
+  </button>
+))
+
+// Knobs as dynamic variables.
+stories.add('as dynamic variables', () => {
+  const name = text('Name', 'Arunoda Susiripala');
+  const age = number('Age', 89);
+
+  const content = `I am ${name} and I'm ${age} years old.`;
+  return (<div>{content}</div>);
+});
