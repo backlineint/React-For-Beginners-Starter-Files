e282878daf5163abdfecd723ff84d3e6745c1519
Brian Perry <contact.brianperry@gmail.com>
2017-07-26 11:47:39 -0500

    Updated routing base to work for both gh-pages and local development

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -10,7 +10,7 @@ import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
 
-const repo = `/${window.location.pathname.split('/')[1]}`;
+const repo = window.location.pathname.split('/')[1] === 'store' ? '/' : `/${window.location.pathname.split('/')[1]}`;
 
 const Root = () => {
   return (
